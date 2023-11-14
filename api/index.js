d6513d7fcc82193bf3d592a030c0c0829284e191
Matheus Felipe Christmann <matheus.ch35@hotmail.com>
2023-11-13 22:27:07 -0300

    Parte 1

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import clienteRoutes from "./routes/clientes.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/", clienteRoutes);
+
+app.listen(3301);
