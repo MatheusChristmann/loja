f89aebd3f523c5415d169fc11c195d261dde0d3d
Matheus Felipe Christmann <matheus.ch35@hotmail.com>
2023-11-17 22:56:31 -0300

    #Ajuste 03

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -4,122 +4,122 @@ import styled from "styled-components";
 import { toast } from "react-toastify";
 
 const FormContainer = styled.form`
-    display: flex;
-    align-items: flex-end;
-    gap: 10px;
-    flex-wrap: wrap;
-    background-color: #fff;
-    padding: 20px;
-    box-shadow: 0px 0px 5px #ccc;
-    border-radius: 5px;
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
 `;
 
 const InputArea = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Input = styled.input`
-    width: 120px;
-    padding: 0 10px;
-    border: 1px solid #bbb;
-    border-radius: 5px;
-    height: 40px;
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
 `;
 
 const Label = styled.label``;
 
 const Button = styled.button`
-    padding: 10px;
-    cursor: pointer;
-    border-radius: 5px;
-    border: none;
-    background-color: #2c73d2;
-    color: white;
-    height: 42px;
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #2c73d2;
+  color: white;
+  height: 42px;
 `;
 
-const Form = ({ getClientes, onEdit, setOnEdit }) => {
-    const ref = useRef();
+const Form = ({ getclientes, onEdit, setOnEdit }) => {
+  const ref = useRef();
 
-    useEffect(() => {
-        if (onEdit) {
-            const cliente = ref.current;
+  useEffect(() => {
+    if (onEdit) {
+      const cliente = ref.current;
 
-            cliente.nome.value = onEdit.nome;
-            cliente.data_nascimento.value = onEdit.data_nascimento;        
-            cliente.fone = onEdit.fone;
-            cliente.email.value = onEdit.email;
-        }
-    }, [onEdit]);
+      cliente.nome.value = onEdit.nome;
+      cliente.email.value = onEdit.email;
+      cliente.fone.value = onEdit.fone;
+      cliente.data_nascimento.value = onEdit.data_nascimento;
+    }
+  }, [onEdit]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const cliente = ref.current;
+    const cliente = ref.current;
 
-        if (
-            !cliente.nome.value ||
-            !cliente.data_nascimento.value ||            
-            !cliente.fone.value ||
-            !cliente.email.value 
-        ) {
-            return toast.warn("Preencha todos os campos!");
-        }
+    if (
+      !cliente.nome.value ||
+      !cliente.email.value ||
+      !cliente.fone.value ||
+      !cliente.data_nascimento.value
+    ) {
+      return toast.warn("Preencha todos os campos!");
+    }
 
-        if (onEdit) {
-            await axios
-            .put("http://localhost:8800/" + onEdit.id, {
-                nome: cliente.nome.value,
-                data_nascimento: cliente.data_nascimento.value,                
-                fone: cliente.fone.value,
-                email: cliente.email.value,
-            })
-            .then(({ data }) => toast.success(data))
-            .catch(({ data }) => toast.error(data));            
-        } else {
-            await axios
-            .post("http://localhost:8800", {
-                nome: cliente.nome.value,
-                data_nascimento: cliente.data_nascimento.value,                
-                fone: cliente.fone.value,
-                email: cliente.email.value,
-            })
-            .then(({ data }) => toast.success(data))
-            .catch(({ data }) => toast.error(data));                
-        }
+    if (onEdit) {
+      await axios
+        .put("http://localhost:8800/" + onEdit.id, {
+          nome: cliente.nome.value,
+          email: cliente.email.value,
+          fone: cliente.fone.value,
+          data_nascimento: cliente.data_nascimento.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    } else {
+      await axios
+        .post("http://localhost:8800", {
+          nome: cliente.nome.value,
+          email: cliente.email.value,
+          fone: cliente.fone.value,
+          data_nascimento: cliente.data_nascimento.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    }
 
-        cliente.nome.value = "";
-        cliente.data_nascimento.value = "";
-        cliente.fone.value = "";
-        cliente.email.value = "";
+    cliente.nome.value = "";
+    cliente.email.value = "";
+    cliente.fone.value = "";
+    cliente.data_nascimento.value = "";
 
-        setOnEdit(null);
-        getClientes();
-    };
+    setOnEdit(null);
+    getclientes();
+  };
 
-    return (
-        <FormContainer ref={ref} onSubmit={handleSubmit}>            
-            <InputArea>
-                <Label>Nome</Label>
-                <Input name="nome" />
-            </InputArea>
-            <InputArea>
-                <Label>Email</Label>
-                <Input name="email" type="email" />
-            </InputArea>
-            <InputArea>
-                <Label>Data Nascimento</Label>
-                <Input name="data_nascimento" type="date" />
-            </InputArea>            
-            <InputArea>
-                <Label>Telefone</Label>
-                <Input name="fone" />
-            </InputArea>            
+  return (
+    <FormContainer ref={ref} onSubmit={handleSubmit}>
+      <InputArea>
+        <Label>Nome</Label>
+        <Input name="nome" />
+      </InputArea>
+      <InputArea>
+        <Label>E-mail</Label>
+        <Input name="email" type="email" />
+      </InputArea>
+      <InputArea>
+        <Label>Telefone</Label>
+        <Input name="fone" />
+      </InputArea>
+      <InputArea>
+        <Label>Data de Nascimento</Label>
+        <Input name="data_nascimento" type="date" />
+      </InputArea>
 
-            <Button type="submit">SALVAR</Button>
-        </FormContainer>
-    )
-}
+      <Button type="submit">SALVAR</Button>
+    </FormContainer>
+  );
+};
 
 export default Form;
